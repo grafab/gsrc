322682348510cb564311815f1142734162e9606b
Fabian Grandke <grafabian@gmail.com>
2016-10-12 16:54:14 +0200

    datasets added for vignette

---
+++ R/data.R
@@ -0,0 +1,37 @@
+#' Brassica napus synteny data
+#'
+#' Synteny alignments created with DECIPHER using publicly available FASTA sequences
+#'
+#' @docType data
+#' @format A data.frame object, consisting of eight columns
+#' \describe{
+#'   \item{index1}{Chromosome index in first subgenome}
+#'   \item{start1}{Position in first subgenome}
+#'   \item{index2}{Chromosome index in second subgenome}
+#'   \item{start2}{Position in second subgenome}
+#'   \item{Alev}{Chromosome level}
+#'   \item{Clev}{Chromosome level}
+#'   \item{AGlo}{Global position}
+#'   \item{CGlo}{Global position}
+#' }
+#'
+"syn_uni"
+
+#' Cotton synteny data
+#'
+#' Synteny alignments created with DECIPHER using publicly available FASTA sequences
+#'
+#' @docType data
+#' @format A data.frame object, consisting of eight columns
+#' \describe{
+#'   \item{index1}{Chromosome index in first subgenome}
+#'   \item{start1}{Position in first subgenome}
+#'   \item{index2}{Chromosome index in second subgenome}
+#'   \item{start2}{Position in second subgenome}
+#'   \item{Alev}{Chromosome level}
+#'   \item{Clev}{Chromosome level}
+#'   \item{AGlo}{Global position}
+#'   \item{CGlo}{Global position}
+#' }
+#'
+"syn_uniC"
